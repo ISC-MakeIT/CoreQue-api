ae680c8ca218b46a3dbc2570c80c4101a24963ea
tamagram <tamagram64@gmail.com>
2021-09-08 13:30:33 +0900

    :sparkles: Create sample code.

--- lambda-scrape/handler.py
+++ lambda-scrape/handler.py
@@ -3,11 +3,41 @@ import sys
 
 sys.path.insert(0, "package/")
 
-import requests
+import json
+import boto3
+import uuid
+import datetime
+from pytz import timezone
+
+table_name = "Meal"
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(table_name)
 
 
 def lambda_handler(event, context):
-
-    my_ip = requests.get("https://api.ipify.org?format=json").json()
-
-    return {"Public Ip": my_ip["ip"]}
+    resp = table.put_item(
+        Item={
+            "Id": str(uuid.uuid4()),
+            "Classification": "riceball",
+            "Name": "tunamayo",
+            "Calorie": 3,
+            "Carbohydrate": 37,
+            "Fat": 3,
+            "Protein": 4,
+            "Sodium": 113,
+            "details": {
+                "Name": "Rice Ball",
+                "Classification": "riceball",
+                "Name": "tunamayo",
+                "Calorie": 3,
+                "Carbohydrate": 37,
+                "Fat": 3,
+                "Protein": 4,
+                "Sodium": 113,
+                "Photo": "https://example.com",
+                "Timestamp": str(datetime.datetime.now(timezone("Asia/Tokyo"))),
+            },
+        }
+    )
+    return {"statusCode": 200, "body": json.dumps(resp)}
