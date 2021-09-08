a57a2917b898b912a3ee0e6494934f0f4abd7b9e
tamagram <tamagram64@gmail.com>
2021-09-08 13:26:36 +0900

    :wrench: Fix build script.

--- lambda-scrape/build.sh
+++ lambda-scrape/build.sh
@@ -4,4 +4,4 @@
 # -- は dirname: invalid option -- 'b' を回避するために使用
 cd "$(dirname -- "$0")"
 pipenv run pip freeze > requirements.txt
-pipenv run pip install -r requirements.txt -t package/
+pipenv run pip install -r requirements.txt -t package/ --upgrade
