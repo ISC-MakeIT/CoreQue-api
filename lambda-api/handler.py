8efa1293448f96c45d094286ed91a675d163fb57
tamagram <tamagram64@gmail.com>
2021-09-11 20:20:36 +0900

    :sparkles: カテゴリごと10件の食品を取得できるようなサンプルコードを追加

--- lambda-api/handler.py
+++ lambda-api/handler.py
@@ -1,9 +1,31 @@
 import sys
+
 sys.path.insert(0, "package/")
 import boto3
+from pprint import pprint
+
+
+def get_meal(dynamodb=None):
+    if not dynamodb:
+        client = boto3.client("dynamodb")
+    try:
+        response = client.query(
+            TableName="Meal",
+            IndexName="MealClassifyIndex",
+            Limit=10,
+            Select="ALL_ATTRIBUTES",
+            KeyConditionExpression="MealClassify = :meal_classify",
+            ExpressionAttributeValues={":meal_classify": {"S": "riceball"}},
+        )
+    except client.exceptions.InternalServerError:
+        return None
+    else:
+        return response["Item"]
+
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda!'
-    }
+    meal = get_meal()
+    if meal:
+        print("取得完了")
+        pprint(meal, sort_dicts=False)
+    return {"statusCode": 200, "body": "Hello from Lambda!"}
