60f1e9334f2163a67628585ff84b681960503f23
tamagram <tamagram64@gmail.com>
2021-09-12 00:17:11 +0900

    :adhesive_bandage: tfファイル修正に伴って気になったところを修正

--- lambda-scrape/build.sh
+++ lambda-scrape/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Change to the script directory
-# -- は dirname: invalid option -- 'b' を回避するために使用
-cd "$(dirname -- "$0")"
+cd `dirname $0`
 pipenv run pip freeze > requirements.txt
 pipenv run pip install -r requirements.txt -t package/ --upgrade
