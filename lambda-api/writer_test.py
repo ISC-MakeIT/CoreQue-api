7e040e0e370774d7c2a5f9773de5a7ab93139354
tamagram <tamagram64@gmail.com>
2021-09-14 03:59:01 +0900

    :memo: 説明を追加

--- lambda-api/writer_test.py
+++ lambda-api/writer_test.py
@@ -7,12 +7,18 @@ class TestWriter(unittest.TestCase):
         self.writer = Writer()
 
     def test_get_response(self):
+        """
+        基本的なレスポンスを返す
+        """
         want = {"statusCode": 200, "body": {"message": "test"}}
         new_writer = Writer(want)
         got = new_writer.get_response()
         self.assertEqual(want, got)
 
     def test_body_write(self):
+        """
+        レスポンスのbodyに書き込む
+        """
         want = {"statusCode": 200, "body": {"message": "test"}}
         body = {"message": "test"}
         self.writer.body_write(body=body)
