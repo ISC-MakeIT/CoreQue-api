6f305ab15636d7b4f00b6a7a62750f41fd29d822
tamagram <tamagram64@gmail.com>
2021-09-13 22:22:00 +0900

    :white_check_mark: シナリオテストの追加

--- lambda-api/route_test.py
+++ lambda-api/route_test.py
@@ -2,6 +2,14 @@ import unittest
 import route
 
 
+class SpyFunctionCalling:
+    def __init__(self):
+        self.called_function_names = []
+
+    def __call__(self, function_name: str):
+        self.called_function_names.append(function_name)
+
+
 class TestRoute(unittest.TestCase):
     def setUp(self):
         self.route = route.Route()
@@ -20,6 +28,19 @@ class TestRoute(unittest.TestCase):
         got = []
         got.append(self.route.handlers["hoge"])
         got.append(self.route.handlers["hoge/fuga"])
+        self.assertEqual(want, got)
+
+    def test_run(self):
+        spy_function_calling = SpyFunctionCalling()
+
+        def hoge():
+            spy_function_calling("hoge")
+
+        self.route.add(path="hoge", func=hoge)
+        self.route.run("hoge")
+
+        want = ["hoge"]
+        got = spy_function_calling.called_function_names
         self.assertEqual(want, got)
 
 
