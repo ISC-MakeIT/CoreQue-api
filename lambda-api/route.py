7ac96370d0e5ac57fbc7db027e33ee70d4354dff
tamagram <tamagram64@gmail.com>
2021-09-14 03:55:18 +0900

    :ballot_box_with_check: シナリオテストをパス

--- lambda-api/route.py
+++ lambda-api/route.py
@@ -1,6 +1,10 @@
+from writer import Writer
+
+
 class Route:
-    def __init__(self, handlers: dict = {}) -> None:
+    def __init__(self, writer: Writer = None, handlers: dict = {}) -> None:
         self.__handlers = handlers
+        self.__writer = writer
 
     def add(self, path: str, func: callable) -> None:
         self.__handlers[path] = func
@@ -10,6 +14,12 @@ class Route:
 
     def run(self, path: str) -> bool:
         if self.has_path(path):
-            self.__handlers[path]()
+            if self.__writer is not None:
+                self.__writer.body_write(self.__handlers[path]())
+            else:
+                self.__handlers[path]()
             return True
         return False
+
+    def get_result(self) -> dict:
+        return self.__writer.get_response()
