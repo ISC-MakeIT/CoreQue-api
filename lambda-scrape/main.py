8306c1a3c4a928d3388eab7105a0a339c81a478c
tamagram <tamagram64@gmail.com>
2021-10-01 04:46:37 +0900

    :bug: uuid取得できてなかった問題を解決

--- lambda-scrape/main.py
+++ lambda-scrape/main.py
@@ -1,14 +1,17 @@
+import uuid
 from model import *
-import json
+import requests
 
 baseURL = "https://www.sej.co.jp/products/a/sandwich/1/l15/"
-item_urls_suffix = get_url_hand_over(baseURL)
+html = requests.get(baseURL).content
+item_urls_suffix = get_url_hand_over(html)
 
-seven_url_prefix = 'https://www.sej.co.jp{}'
+seven_url_prefix = "https://www.sej.co.jp{}"
 resp = []
 for suffix in item_urls_suffix:
     url = seven_url_prefix.format(suffix)
     timestamp = str(datetime.datetime.now(timezone("Asia/Tokyo")))
+    id = str(uuid.uuid4())
     resp.append(get_nutrition(url, id, timestamp))
 
 print(resp)
