3f9dd48cf2a6902a4ee45163dc13562937230934
tamagram <tamagram64@gmail.com>
2021-10-02 10:44:10 +0900

    :fire: 不要なコードを削除

--- lambda-api/handler.py
+++ lambda-api/handler.py
@@ -6,18 +6,6 @@ from boto3.dynamodb.conditions import Key
 from pprint import pprint
 import json
 
-
-# def get_meal(dynamodb=None):
-#     if not dynamodb:
-#         dynamodb = boto3.resource("dynamodb")
-#     table = dynamodb.Table("Meal")
-
-#     response = table.query(
-#         IndexName="MealClassifyIndex",
-#         KeyConditionExpression=Key("Classification").eq("riceball"),
-#     )
-#     return response["Items"]
-
 from route import Route
 from writer import Writer
 
@@ -55,12 +43,6 @@ route.add(path="onigiri", func=onigiri)
 
 
 def lambda_handler(event, context):
-    # meal = get_meal()
-    # if meal:
-    #     print("取得完了")
-    #     pprint(meal, sort_dicts=False)
-    # return {"statusCode": 200, "body": json.dumps(meal, indent=2)}
-
     route.run(path=event["pathParameters"]["proxy"])
     resp = route.get_result()
     return resp
