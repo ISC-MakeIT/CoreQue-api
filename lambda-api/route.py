86fd6f4438a84eb57dd41f8388563db2ca07bcde
tamagram <tamagram64@gmail.com>
2021-10-02 23:38:47 +0900

    :ballot_box_with_check: テストをパス

--- lambda-api/route.py
+++ lambda-api/route.py
@@ -6,20 +6,30 @@ class Route:
         self.__handlers = handlers
         self.__writer = writer
 
-    def add(self, path: str, func: callable) -> None:
-        self.__handlers[path] = func
+    def add(self, path: str, func: callable, key: str = None) -> None:
+        if key is not None:
+            self.__handlers[path] = {"key": key, "func": func}
+        else:
+            self.__handlers[path] = {"key": None, "func": func}
 
     def has_path(self, path: str) -> bool:
         return path in self.__handlers
 
-    def run(self, path: str) -> bool:
-        if self.has_path(path):
-            if self.__writer is not None:
-                self.__writer.body_write(self.__handlers[path]())
-            else:
-                self.__handlers[path]()
-            return True
-        return False
+    def run(self, path: str, param: dict = None) -> bool:
+        if not self.has_path(path):
+            return False
+        if param is None:
+            if self.__writer is None:
+                self.__handlers[path]["func"]()
+                return True
+            self.__writer.body_write(self.__handlers[path]["func"]())
+        else:
+            key = self.__handlers[path]["key"]
+            if self.__writer is None:
+                self.__handlers[path]["func"](param[key])
+                return True
+            self.__writer.body_write(self.__handlers[path]["func"](param[key]))
+        return True
 
     def get_result(self) -> dict:
         return self.__writer.get_response()
