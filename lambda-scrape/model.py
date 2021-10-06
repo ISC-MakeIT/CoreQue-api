e421bfb143192092e801b969ec20a7ddb766c772
tamagram64 <tamagram64@gmail.com>
2021-10-06 18:54:17 +0900

    :ballot_box_with_check: テストをパス

--- lambda-scrape/model.py
+++ lambda-scrape/model.py
@@ -27,34 +27,31 @@ def get_url_hand_over(html: bytes) -> list:
     return result
 
 
-def get_nutrition(url: str, classification: str, timestamp: str) -> dict:
+def get_nutrition(html: bytes, classification: str, timestamp: str) -> dict:
     def str_to_int(value: str) -> int:
         return int(float(value))
 
-    res = requests.get(url)
-    if res.status_code != 200:
-        return False
-    soup = BeautifulSoup(res.content, "html.parser")
-
-    td = soup.find_all("td")
-    nutrient_component = td[1].text
-
-    nutrition = []
-    column = re.sub(r"[g|kcal]", ",", nutrient_component)
-    b = re.split(r"（|）|kcal |、", column)
-    d = ",".join(b)
-    c = d.split("," "")
-    strandint = list(filter(None, c))
-    for i in strandint:
-        Coordinate = i.find("：") + 1
-        onlyNum = i[Coordinate:]
-        nutrition.append(onlyNum)
+    soup = BeautifulSoup(html, "html.parser")
     try:
+        td = soup.find_all("td")
+        nutrient_component = td[1].text
+
+        nutrition = []
+        column = re.sub(r"[g|kcal]", ",", nutrient_component)
+        b = re.split(r"（|）|kcal |、", column)
+        d = ",".join(b)
+        c = d.split("," "")
+        strandint = list(filter(None, c))
+        for i in strandint:
+            Coordinate = i.find("：") + 1
+            onlyNum = i[Coordinate:]
+            nutrition.append(onlyNum)
+
         # FIXME: ハッシュ値をIDとする
         name = soup.find("h1").text
-        id = hashlib.sha256(name.encode()).hexdigest()
+        id = hashlib.sha256(name.encode("utf-8")).hexdigest()
         item = {
-            "Id": hashlib.sha256(b"Python").hexdigest(),
+            "Id": id,
             "Classification": classification,
             "Name": name,
             "Calorie": str_to_int(nutrition[0]),
